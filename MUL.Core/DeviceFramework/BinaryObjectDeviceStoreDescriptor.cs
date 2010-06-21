f2aff0af8f09b92c353118a3c114bfbd37f86ba4
Simon Wollwage <kintaro@think-in-co.de>
2010-06-21 05:53:43 +0200

    - Added BOS Descriptor and ContainerID Descriptor

---
+++ MUL.Core/DeviceFramework/BinaryObjectDeviceStoreDescriptor.cs
@@ -0,0 +1,33 @@
+using System;
+namespace MUL.Core.DeviceFramework
+{
+	/// <summary>
+	/// 	The BOS descriptor defines a root descriptor that is 
+	/// 	similar to the configuration descriptor, and is the 
+	/// 	base descriptor for accessing a family of related descriptors. 
+	/// 	A host can read a BOS descriptor and learn from the 
+	/// 	TotalLength field the entire size of the device-level 
+	/// 	descriptor set, or it can read in the entire BOS descriptor 
+	/// 	set of device capabilities.
+	/// </summary
+	public class BinaryObjectDeviceStoreDescriptor
+	{
+		/// <summary>
+		/// 	Size of descriptor
+		/// </summary>
+		public byte Number { get; set; }
+		/// <summary>
+		/// 	BOS Descriptor type
+		/// </summary>
+		public byte DescriptorType { get; set; }
+		/// <summary>
+		/// 	Length of this descriptor and all of its sub descriptors
+		/// </summary>
+		public ushort TotalLength { get; set; }
+		/// <summary>
+		/// 	The number of separate device capability descriptors in the BOS
+		/// </summary>
+		public byte NumberOfDeviceCapabilityDescriptors { get; set; }
+	}
+}
+
---
+++ MUL.Core/DeviceFramework/ContainerIdDescriptor.cs
@@ -0,0 +1,41 @@
+using System;
+namespace MUL.Core.DeviceFramework
+{
+	/// <summary>
+	/// 	This descriptor may be used by a host in order to 
+	/// 	identify a unique device instance across all operating 
+	/// 	modes. If a device can also connect to a host through 
+	/// 	other technologies, the same Container ID value contained 
+	/// 	in this descriptor should also be provided over those other 
+	/// 	technologies in a technology specific manner.
+	/// </summary>
+	public class ContainerIdDescriptor
+	{
+		/// <summary>
+		/// 	Size of descriptor
+		/// </summary>
+		public byte Length { get; set; }
+		/// <summary>
+		/// 	DEVICE CAPABILITY Descriptor type
+		/// </summary>
+		public byte DescriptorType { get; set; }
+		/// <summary>
+		/// 	Capability type: CONTAINER_ID
+		/// </summary>
+		public byte DeviceCapabilityDescriptor { get; set; }
+		/// <summary>
+		/// 	This field is reserved and shall be set to zero.
+		/// </summary>
+		public byte Reserved { get; set; }
+		/// <summary>
+		/// 	This is a 128-bit number that is unique to a device 
+		/// 	instance that is used to uniquely identify the device 
+		/// 	instance across all modes of operation. This same value 
+		/// 	may be provided over other technologies as well to allow 
+		/// 	the host to identify the device independent of means
+		/// 	of connectivity.
+		/// </summary>
+		public Guid ContainerID { get; set; }
+	}
+}
+
